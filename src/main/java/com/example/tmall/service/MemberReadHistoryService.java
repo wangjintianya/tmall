78f74d1345427c491ea5e4580f30a7daf694ae4a
wangjintianya <601221052@qq.com>
2020-11-02 19:43:37 +0800

    整合mongodb实现文档操作

---
+++ src/main/java/com/example/tmall/service/MemberReadHistoryService.java
@@ -0,0 +1,22 @@
+package com.example.tmall.service;
+
+import com.example.tmall.nosql.mongodb.document.MemberReadHistory;
+
+import java.util.List;
+
+public interface MemberReadHistoryService {
+    /**
+     * 生成浏览记录
+     */
+    int create(MemberReadHistory memberReadHistory);
+
+    /**
+     * 批量删除浏览记录
+     */
+    int delete(List<String> ids);
+
+    /**
+     * 获取用户浏览历史记录
+     */
+    List<MemberReadHistory> list(Long memberId);
+}
---
+++ src/main/java/com/example/tmall/service/impl/MemberReadHistoryServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.tmall.service.impl;
+
+import com.example.tmall.nosql.mongodb.document.MemberReadHistory;
+import com.example.tmall.nosql.mongodb.repository.MemberReadHistoryRepository;
+import com.example.tmall.service.MemberReadHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class MemberReadHistoryServiceImpl implements MemberReadHistoryService {
+    @Autowired
+    private MemberReadHistoryRepository memberReadHistoryRepository;
+
+    @Override
+    public int create(MemberReadHistory memberReadHistory) {
+        memberReadHistory.setId(null);
+        memberReadHistory.setCreateTime(new Date());
+        memberReadHistoryRepository.save(memberReadHistory);
+
+        return 1;
+    }
+
+    @Override
+    public int delete(List<String> ids) {
+        List<MemberReadHistory> deleteList = new ArrayList<>();
+        for (String id : ids) {
+            MemberReadHistory memberReadHistory = new MemberReadHistory();
+            memberReadHistory.setId(id);
+            deleteList.add(memberReadHistory);
+        }
+
+        memberReadHistoryRepository.deleteAll(deleteList);
+        return 1;
+    }
+
+    @Override
+    public List<MemberReadHistory> list(Long memberId) {
+        return memberReadHistoryRepository.findByMemberIdOrderByCreateTimeDesc(memberId);
+    }
+}
